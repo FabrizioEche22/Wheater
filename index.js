f62031e1b30c30efb620ae53bb0722fd9a08513a
Fabrizio Eche <mifaroeche22@gmail.com>
2024-05-10 16:59:03 -0500

    Dependencia y api del clima

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hola Mundo");
+const weather = require('weather-js');
+
+const data = {
+  ubicacion: "Lima",
+}
+
+weather.find({
+  search: data.ubicacion,
+  degreeType: 'C',
+},
+
+  function(err, result) {
+    if (err) console.log(err);
+    let resultadoFinal = JSON.stringify(result, null, 2);
+    console.log(resultadoFinal);
+  });
