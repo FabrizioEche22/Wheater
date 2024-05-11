edac3d76dbdfa0880801da7bfeae494389732912
Fabrizio Eche <mifaroeche22@gmail.com>
2024-05-11 11:39:18 -0500

    Busqueda del clima funcional

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ const PORT = 3000;
 app.use(express.static('public'));
 
 app.get('/buscar', (req, res) => {
+  const ciudad = req.query.ciudad;
+  console.log(ciudad)
   weather.find({
-    search: "Lima",
+    search: ciudad,
     degreeType: 'C',
   },
   
   function (err, result) {
       if (err) {
         console.log(err);
+        res.status(500).send('Error interno del servidor');
       } else {
         let nombre = result[0].location?.name || 'Nada';
         let temperatura = result[0].current?.temperature || 'Nada';
