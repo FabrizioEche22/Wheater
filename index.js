ceff2625420394d87eb89ba1bb7540c3536ca2cd
Fabrizio Eche <mifaroeche22@gmail.com>
2024-05-14 13:57:13 -0500

    Mejoras, restricciones y añadida de opciones con estilos y funcional.

--- index.js
+++ index.js
@@ -5,27 +5,31 @@ const PORT = 3000;
 
 app.use(express.static('public'));
 
-app.get('/buscar', (req, res) => {
-  const ciudad = req.query.ciudad;
-  weather.find({
-    search: ciudad,
-    degreeType: 'C',
-  },
+try {
+  app.get('/buscar', (req, res) => {
+    const ciudad = req.query.ciudad;
+    weather.find({
+      search: ciudad,
+      degreeType: 'C',
+    },
+    
+    function (err, result) {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Error interno del servidor');
+        } else {
+          let nombre = result[0].location?.name;
+          let temperatura = result[0].current?.temperature;
+          let image = result[0].current?.imageUrl;
+          let p = result[0].current?.skytext;
   
-  function (err, result) {
-      if (err) {
-        console.log(err);
-        res.status(500).send('Error interno del servidor');
-      } else {
-        let nombre = result[0].location?.name;
-        let temperatura = result[0].current?.temperature;
-        let image = result[0].current?.imageUrl;
-        let p = result[0].current?.skytext;
-
-        res.json({ nombre, temperatura, image, p });
-      }
-  });
-})
+          res.json({ nombre, temperatura, image, p });
+        }
+    });
+  })
+} catch (err) {
+  console.log(err);
+}
 
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en: http:localhost:${PORT}`);
--- public/index.js
+++ public/index.js
@@ -1,3 +1,15 @@
+function abrirConfig() {
+  let config = document.getElementById('config');
+  let toolIcon = document.getElementById('icon');
+  if(config.style.display === 'none') {
+    config.style.display = 'block';
+    toolIcon.style.transform = 'rotate(-126deg)';
+  } else {
+    config.style.display = 'none';
+    toolIcon.style.transform = 'rotate(126deg)';
+  }
+}
+
 function enter(event) {
   if (event.keyCode === 13) {
     var inputValue = document.getElementById('input').value;
